c1aa1ae272c4345e7bca2305efaa4f03b8733011
J Rhee <jayrhee0514@gmail.com>
2021-01-07 23:27:39 -0500

    installed axios, and added a helloworld function to test it out

--- employee-directory/src/components/MainSearch/mainsearch.js
+++ employee-directory/src/components/MainSearch/mainsearch.js
@@ -1,2 +1,9 @@
-import React, {useState, useEffect, Component} from "react";
+import React, { useState, useEffect, Component } from "react";
 import API from "../../utils/API";
+
+
+function MainSearchPage() {
+    return <div>Hello World</div>
+}
+
+export default MainSearchPage
