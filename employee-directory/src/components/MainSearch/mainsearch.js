942dc7f10c1100f8baaa9ac93ec1debb3526ae8d
J Rhee <jayrhee0514@gmail.com>
2021-01-08 00:33:40 -0500

    found a MDBreact as a good way to make a datatable. trying to figure it out.

--- employee-directory/src/components/MainSearch/mainsearch.js
+++ employee-directory/src/components/MainSearch/mainsearch.js
@@ -1,5 +1,20 @@
 import React, { useState, useEffect, Component } from "react";
 import API from "../../utils/API";
+import { MDBDataTableV5 } from 'mdbreact';
+
+const columns= [
+    {
+        label: "Image",
+        field: "image",
+        width: 150,
+    }
+]
+
+
+
+
+
+
 
 
 function MainSearchPage() {
