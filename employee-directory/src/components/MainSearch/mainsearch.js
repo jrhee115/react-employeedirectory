73f54420fd4ba6beb4b18ceb8f728ab7ce2bfd14
J Rhee <jayrhee0514@gmail.com>
2021-01-10 18:18:24 -0500

    changed my 'useeffect' code since my app wasnt running, modified my api

--- employee-directory/src/components/MainSearch/mainsearch.js
+++ employee-directory/src/components/MainSearch/mainsearch.js
@@ -1,68 +1,57 @@
-import React, {useState, useEffect} from "react";
+import React, { useEffect } from "react";
 import API from "../../utils/API";
 import { MDBDataTableV5 } from 'mdbreact';
 
-const columns= [
-    {
-        label: "Image",
-        field: "image",
-        width: 150,
-    },
-    {
-        label: "Name",
-        field: "name",
-        width: 200,
-    },
-    {
-        label: "Phone",
-        field: "phone",
-        width: 100,
-    },
-    {
-        label: "Email",
-        field: "email",
-        width: 200,
-    },
-    {
-        label: "DOB",
-        field: "dob",
-        width: 100,
-    },
-]
-
-export default function mainSearch(){
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [datatable, setDatatable] = useState({})
-
-    useEffect(() =>{
-        API.populate()
-        .then(res => {
-            console.log("res: ", res.data.results);
-            setUsers(res.data.results);
-            console.log("Users: ", users);
-            const rows = users.map((user, i ) => {
-                return {
-                    image: <img src={user.picture.medium} alt="employee image"/>,
-                    name: user.name.first + " " + user.name.last,
-                    phone: user.cell,
-                    email: user.email,
-                    dob: user.dob.date.split("T")[0]
-                }
-            })
-
-            
-        })
+export default function mainSearch() {
+    const [datatable, setDatatable] = React.useState({
+        columns: [
+            {
+                label: "Image",
+                field: "image",
+                width: 150,
+            },
+            {
+                label: "Name",
+                field: "name",
+                width: 200,
+            },
+            {
+                label: "Phone",
+                field: "phone",
+                width: 100,
+            },
+            {
+                label: "Email",
+                field: "email",
+                width: 200,
+            },
+            {
+                label: "DOB",
+                field: "dob",
+                width: 100,
+            },
+        ]
     })
 
 
+    useEffect(() => {
+        API.getUsers()
+            .then(res => {
+                console.log(res);
+                const rows = res.data.results.map((user ) => {
+                    return {
+                        image: <img src={user.picture.medium} alt="employee image" />,
+                        name: user.name.first + " " + user.name.last,
+                        phone: user.cell,
+                        email: user.email,
+                        dob: user.dob.date.split("T")[0]
+                    }
+                })
 
+                setDatatable((originalState) => {
+                    return {...originalState, rows: rows}
+                });
+            })
+    },[])
     return <MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={datatable} />;
 }
-
-
-// function MainSearchPage() {
-//     return <div>Hello World</div>
-// }
-
-// export default MainSearchPage
--- employee-directory/src/index.js
+++ employee-directory/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "mdbreact/dist/css/mdb.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
